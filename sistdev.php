9a2753957698822fe6c89c589c63de941fcb03b3
antonystar-dev <antonystar.contatos@gmail.com>
2023-12-14 07:12:10 +0000

    remoção do bootstrap e css antigo.

--- sistdev.php
+++ sistdev.php
@@ -1,31 +1,11 @@
-<span id="sistd"><br><br><br><br><br></span>
-        <h2>Sistemas desenvolvidos</h2>
-
-        <div class="col-12 container ">
-            <!---excluir daqui
-            <div>
-                <button type="button"  onclick="Mudarestado('sistema1')" class="btn btn-primary">WPC</button>
-                <button type="button"  onclick="Mudarestado('sistema2')" class="btn btn-danger">+ SAUDE</button>
-                <button type="button"  onclick="Mudarestado('sistema3')" class="btn btn-success">2 FATORES</button>
-            </div>
-           
-            <br>
-            <p>Escolha algum sistema acima para mais informações:</p>
-             -excluir ate aq-->
-            <div class="row">
-
                 <!---SISTEMA 1-->
                
-                <div  class="col-4 text-white">
-                    <div class="sist1">
+                
                         <h4 class="text-white">LOGIN DE 2 FATORES</h4>
                         <span class="txtPedaco">Função:</span>
                         <p>Realizar a autenticação com verificação de 2 fatores dos usuarios cadastrados, utilizando o e-mail como alternativa</p>
                         <span class="txtPedaco">Tecnologias:</span>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                            <span class="col-2 list-group-item">&nbsp;</span>
+                     
                                     <span class="col-2 list-group-item"><img src="img/html-logo.png" width="30px"></span>
                                     <span class="col-2 list-group-item"><img src="img/css-logo.png" width="33px"></span>
                                    
@@ -34,37 +14,22 @@
                                     </span>
                                     <span class="col-2 list-group-item"><img src="img/mysql.png" width="60px"></span>
                                 
-                            </div>
-                        </div>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-12">
+                           
+                     
                                     <span class="txtPedaco">Tipo:</span>
                                     <span>Pessoal</span>
-                                </div>
-                                
-                                
-
-                            </div>
-                        </div>
-                        <div class="col-12">
+                              
                                     <span class="txtPedaco">Link:</span>
                                     <span class="link"><a href="https://2fatores.antonystar.com/"
                                             target="_blank">2fatores.antonystar.com</a></span>
-                                </div>
+                           
                         <hr />
 
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="txtPedaco">Login </div>
-                                <div class="col-12">
+                     Login
+                                
                                     <span> Olá visitante, nessa etapa será necessario que faça o cadastro no sistema para testar as funcionalidades. O nosso banco de dados é resetado a cada 5 horas e não divulgamos suas informações</span>
                                     
-                                </div>
-
-                            </div>
-                        </div>
+                              
                         <hr />
                         <span class="txtPedaco">Repositório GitHub:</span>
                         <span class="link"><a href="https://github.com/antonystar-dev/autenticacaodoisfatores"
@@ -73,159 +38,13 @@
                         <span class="txtPedaco">Atualizações futuras:</span><br><span> Pretendo reestruturar o sistema removendo o carregamento do front-end pelo javascript, e atualizar os links com microserviços em JAVA ou PHP</span>
                         <div class="alerta">EM DESENVOLVIMENTO</div>
 
-                    </div>
-                </div>
+                  
 
                    <!---SISTEMA 2-->
-               
-                   <div  class="col-4 text-black">
-                    <div class="sist2">
-                        <h4 class="text-black">MAIS SAÚDE</h4>
-                        <span class="txtPedaco">Função:</span>
-                        <p>Plataforma na qual exibe a disponibilidade dos serviços nas unidades de saúde do Distrito Federal.</p>
-                        <span class="txtPedaco">Tecnologias:</span>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                                
-                                    <span class="col-2 list-group-item"><img src="img/html-logo.png" width="30px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/css-logo.png" width="33px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/bootstrap-logo-vector.svg"
-                                            width="35px">                                    </span>
-                                    <span class="col-2 list-group-item"><img src="img/js-logo.png" width="30px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/new-php-logo.svg" width="55px">
-                                    </span>
-                                    <span class="col-2 list-group-item"><img src="img/mysql.png" width="60px"></span>
-                                
-                            </div>
-                        </div>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-12">
-                                    <span class="txtPedaco">Tipo:</span>
-                                    <span>TCC</span>
-                                </div>
-                                
-                                
-
-                            </div>
-                        </div>
-                        <div class="col-12">
-                                    <span class="txtPedaco">Link:</span>
-                                    <span class="link"><a href="https://maissaude.antonystar.com/"
-                                            target="_blank">maissaude.antonystar.com</a></span>
-                                </div>
-                        <hr />
-
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="txtPedaco">Login administrativo</div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Usuario:</span>
-                                    <p>admin</p>
-                                </div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Senha:</span>
-                                    <p>senha123</p>
-                                </div>
-                                <div class="txtPedaco">Login funcionario</div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Usuario:</span>
-                                    <p>funcionario</p>
-                                </div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Senha:</span>
-                                    <p>senha123</p>
-                                </div>
-
-                            </div>
-                        </div>
-                        <hr />
-                        <span class="txtPedaco">Repositório GitHub:</span>
-                        <span class="link"><a href="https://github.com/antonystar-dev/wpc"
-                                target="_blank">CLIQUE AQUI</a></span>
-                        <br>
-                        <span class="txtPedaco">Atualizações futuras:</span><span> Não pretendo atualiza-lo, mas sua base serve de expiração para os demais sistemas projetados.</span>
-
-
-                    </div>
-                </div>
-                    <!---SISTEMA 3-->
-               
-                    <div  class="col-4 text-white">
-                    <div class="sist1">
-                        <h4 class="text-white">WEB PORTAL CURSOS:</h4>
-                        <span class="txtPedaco">Função:</span>
-                        <p>Plataforma na qual agrupa links de afiliados para venda de cursos, podendo genciar links
-                            promocionais, padrões e venda direta.</p>
-                        <span class="txtPedaco">Tecnologias:</span>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                                
-                                    <span class="col-2 list-group-item"><img src="img/html-logo.png" width="30px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/css-logo.png" width="33px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/bootstrap-logo-vector.svg"
-                                            width="35px">                                    </span>
-                                    <span class="col-2 list-group-item"><img src="img/js-logo.png" width="30px"></span>
-                                    <span class="col-2 list-group-item"><img src="img/new-php-logo.svg" width="55px">
-                                    </span>
-                                    <span class="col-2 list-group-item"><img src="img/mysql.png" width="60px"></span>
-                                
-                            </div>
-                        </div>
-                        <br><br>
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-12">
-                                    <span class="txtPedaco">Tipo:</span>
-                                    <span>Pessoal</span>
-                                </div>
-                                
-                                
-
-                            </div>
-                        </div>
-                        <div class="col-12">
-                                    <span class="txtPedaco">Link:</span>
-                                    <span class="link"><a href="https://wpc.antonystar.com/"
-                                            target="_blank">wpc.antonystar.com</a></span>
-                                </div>
-                        <hr />
-
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="txtPedaco">Login administrativo</div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Usuario:</span>
-                                    <p>admin</p>
-                                </div>
-                                <div class="col-6">
-                                    <span class="txtPedaco">Senha:</span>
-                                    <p>senha123</p>
-                                </div>
-                                
-
-                            </div>
-                        </div>
-                        <hr />
-                        <span class="txtPedaco">Repositório GitHub:</span>
-                        <span class="link"><a href="https://github.com/antonystar-dev/wpc"
-                                target="_blank">CLIQUE AQUI</a></span>
-                        <br>
-                        <span class="txtPedaco">Atualizações futuras:</span><span> Pretendo atualizar para a versão 2.0
-                            com a inclusão de perfils individuais, transformando em diversos sites diferentes e fazer a separação do crud em API</span>
-
-
-                    </div>
-                </div>
-
-
+         
                <!-- fim SISTEMAS-->
-               <br><br><br><br>
-               <div class="col-12 alertaBanco">
-                <span>Os bancos de dados dos projetos são resetados para o padrão a cada 5 horas na qual podem realizar testes nos sistemas para verificar suas funcionalidades. Não é permitido o uso dos sistemas para outras atividades que não seja para teste e avaliação dos sistemas. Não cadastrar informações sensiveis, visto que qualquer pessoa pode consultar dentro das 5 horas vigente. O sistema 2Fatores não disponibiliza informações de outros usuarios, portanto o cadastro feito no sistema te disponibilizara somente a conta de usuario comum.</span>
-               </div>
              
-            </div>
+                <span>Os bancos de dados dos projetos são resetados para o padrão a cada 5 horas na qual podem realizar testes nos sistemas para verificar suas funcionalidades. Não é permitido o uso dos sistemas para outras atividades que não seja para teste e avaliação dos sistemas. Não cadastrar informações sensiveis, visto que qualquer pessoa pode consultar dentro das 5 horas vigente. O sistema 2Fatores não disponibiliza informações de outros usuarios, portanto o cadastro feito no sistema te disponibilizara somente a conta de usuario comum.</span>
+           
+             
+       
