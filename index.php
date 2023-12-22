9f6c76a9409c785ffaed73a472906ff9d7789d6c
antonystar-dev <antonystar.contatos@gmail.com>
2023-12-22 05:06:39 +0000

    organização dos arquivos

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+
+<head>
+<?php
+        include_once 'pg/header.html';
+    ?>
+    
+</head>
+
+<body>
+    <?php
+        include_once 'pg/menu.html';
+    ?>
+      <?php
+        include_once 'pg/home.html';
+    ?>
+      <?php
+        include_once 'pg/sobre.html';
+    ?>
+      <?php
+        include_once 'pg/projetos.html';
+    ?>
+      <?php
+        include_once 'pg/sistemas.html';
+    ?>
+      <?php
+        include_once 'pg/tecnologias.html';
+    ?>
+      <?php
+        include_once 'pg/rodape.html';
+    ?>
+
+</body>
+
+</html>
